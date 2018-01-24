174322b6bd970671e0eb2b2286c81a522d8dca30
KalpanaR-hexaware <kalpanaR@hexaware.com>
2018-01-24 12:00:40 +0530

    commit

---
+++ src/main/java/com/vaishali/Square.java
@@ -0,0 +1,12 @@
+package com.vaishali;
+
+
+public class Square
+
+
+{
+   public void calc(int a)
+   {
+       System.out.println(a*a);
+   } 
+}
---
+++ src/test/java/com/vaishali/SquareTest.java
@@ -0,0 +1,13 @@
+package com.vaishali;
+import org.junit.Test;
+
+public class SquareTest
+    {
+@Test
+public void testSquare()
+{
+Square s=new Square();
+s.calc(10);
+    }
+
+    }
